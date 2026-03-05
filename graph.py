e2b44919cec27c4bb44cf5f98bfcd3a6182c8c98
Quentin <quentin.prunet@epita.fr>
2026-03-05 15:00:48 +0100

    feat: LLM API call with Groq (qwen)

--- graph.py
+++ graph.py
@@ -1,13 +1,28 @@
 from typing import List, TypedDict, Dict
-from langchain_core.messages import BaseMessage
+from langchain_groq import ChatGroq
+from langchain_core.messages import AIMessage
 
 class AgentState(TypedDict):
     commodity_name: str
-    search_result: List[BaseMessage]
+    search_result: List[AIMessage]
     dependency_graph: Dict
+
+llm = ChatGroq(
+    model="qwen/qwen3-32b",
+    max_tokens=None,
+)
 
 def search(state: AgentState) -> AgentState:
     """ Search Agent Node for supply lines of the commodity"""
+    message = [
+            (
+                "system",
+                f"You are a supply line analyst. Your task is to search for the supply lines of {state['commodity_name']} and provide relevant information.",
+            )
+    ]
+
+    res = llm.invoke(message)
+    state["search_result"].append(res)
     return state
 
 def impact_agent(state: AgentState) -> AgentState:
--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from graph import search, build_dependencies, AgentState
 if __name__ == "__main__":
     graph = StateGraph(AgentState)
     graph.add_node('search', search)
-    graph.add_node('build_dependencies', build_dependencies)
     graph.set_entry_point('search')
-    graph.set_finish_point('build_dependencies')
+    graph.set_finish_point('search')
     app = graph.compile()
     res = app.invoke({
-        'commodity_name': 'steel',
+        'commodity_name': 'copper',
         'search_result': [],
         'dependency_graph': {}
     })
-    print(res)
+    print(res["search_result"])
